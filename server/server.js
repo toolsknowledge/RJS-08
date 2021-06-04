ac8167805b95c9188057a593b01c81af24677b2a
Samba <samba.pendela@gmail.com>
2021-06-04 21:51:20 +0530

    code

--- server/server.js
+++ server/server.js
@@ -1,53 +1,65 @@
 //nodejs
 //javascript
 
-//import the node modules
+//import the modules
+//@express      @cors       @mongodb        @body-parser
 //require() function, used to import the modules
 const express = require("express");
-const mongodb = require("mongodb");
 const cors = require("cors");
+const mongodb = require("mongodb");
 const bodyparser = require("body-parser");
 
 
-//rest object
+//create the rest object
 const app = express();
 //where "app" is the rest object
-//where "app" object used to develop the rest services like GET,POST,PUT,DELETE,......
+//where "app" object, used to develop the rest services
 
 
 
-//enable cors policy
-app.use( cors() );
+//enable the cors policy
+app.use(cors());
 
 
-
-//MIME Type
+//set the json as MIME type
 app.use(bodyparser.json());
 
 
 
-
-//read the client data
-app.use( bodyparser.urlencoded({extended:false}) );
-
+//read the json data
+app.use(bodyparser.urlencoded({extended:false}));
 
 
 //create the reference variable to connect to mongodb database
 const ashokIT = mongodb.MongoClient;
+//where "ashokIT" is the reference variable
+//where "ashokIT" used to connect to mongodb database
 
 
-
-//create the GET Request
+//create the post request
 app.post("/login",(req,res)=>{
-
+     ashokIT.connect("mongodb+srv://admin:admin@cluster0.jgnmk.mongodb.net/R08?retryWrites=true&w=majority",
+                            (err,connection)=>{
+            if(err) throw err;
+            else{
+                const db = connection.db("R08");
+                db.collection("login_details").find({"username":req.body.username,"password":req.body.password})
+                    .toArray((err,array)=>{
+                        if(err) throw err;
+                        else{
+                            if(array.length>0){
+                                res.send({"login":"success"});
+                            }else{
+                                res.send({"login":"fail"});
+                            }
+                        }
+                });
+            }
+     })
 });
 
 
-
-
-
-
-
-
-
-
+//assign the port number
+app.listen(8080,()=>{
+    console.log("server listening the port number 8080");
+});
